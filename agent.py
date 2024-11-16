9666868bd4ecfced9a7152c1730f35ae04d8ad27
Sankalp Singh <singhsankalp997@gmail.com>
2024-11-16 19:13:46 +0530

    assistant added

--- agent.py
+++ agent.py
@@ -69,5 +69,25 @@ def create_tool_node_with_fallback(tools: list) -> dict:
         exception_key="error"
     )
 
-
+class State(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
         
+
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State):
+        while True:
+            result = self.runnable.invoke(state)
+            if not result.tool_calls and (
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with a real output.")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
+    
