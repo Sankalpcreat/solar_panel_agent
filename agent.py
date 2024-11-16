56147835075e66cece4e700fd4f01e6e20aa9349
Sankalp Singh <singhsankalp997@gmail.com>
2024-11-16 18:59:17 +0530

    first commit

---
+++ agent.py
@@ -0,0 +1,73 @@
+import requests
+from langchain_core.tools import tool
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph.message import AnyMessage, add_messages
+from langchain_core.messages import ToolMessage
+from langchain_core.runnables import RunnableLambda
+from langgraph.prebuilt import ToolNode
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import END, StateGraph, START
+from langgraph.prebuilt import tools_condition
+
+@tool
+def compute_savings(monthly_cost:float)->float:
+    def calculate_solar_savings(monthly_cost):
+
+        cost_per_kWh = 0.28  
+        cost_per_watt = 1.50  
+        sunlight_hours_per_day = 3.5  
+        panel_wattage = 350  
+        system_lifetime_years = 10  
+
+        # Monthly electricity consumption in kWh
+        monthly_consumption_kWh = monthly_cost / cost_per_kWh
+        
+        # Required system size in kW
+        daily_energy_production = monthly_consumption_kWh / 30
+        system_size_kW = daily_energy_production / sunlight_hours_per_day
+        
+        # Number of panels and installation cost
+        number_of_panels = system_size_kW * 1000 / panel_wattage
+        installation_cost = system_size_kW * 1000 * cost_per_watt
+        
+        # Annual and net savings
+        annual_savings = monthly_cost * 12
+        total_savings_10_years = annual_savings * system_lifetime_years
+        net_savings = total_savings_10_years - installation_cost
+
+        return {
+            "number_of_panels": round(number_of_panels),
+            "installation_cost": round(installation_cost, 2),
+            "net_savings_10_years": round(net_savings, 2)
+        }
+
+    # Return calculated solar savings
+    return calculate_solar_savings(monthly_cost)
+
+def handle_tool_error(state)->dict:
+    error=state.get("error")
+    tool_calls=state["message"][-1].tool_calls
+
+    return {
+        "messages": [
+            ToolMessage(
+                content=f"Error: {repr(error)}\n please fix your mistakes.",
+                tool_call_id=tc["id"],
+            )
+            for tc in tool_calls
+        ]
+    }
+
+def create_tool_node_with_fallback(tools: list) -> dict:
+   
+    return ToolNode(tools).with_fallbacks(
+        [RunnableLambda(handle_tool_error)],
+        exception_key="error"
+    )
+
+
+        
